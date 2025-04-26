e6c53a946e151cce8526bb2344672e096bcc6dfe
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-26 20:44:42 +0200

    refactor:working on the database connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import express from 'express'
 import router from "./router";
+import db from './config/db';
 
+// conectar a base de datos
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('Conexión exitosa a la BD');
+        
+    } catch (error) {
+        console.log('Hubo un error al conectar a la BD')
+        console.log(error);
+    }
+}
+
+connectDB()
 const server = express()
 
 server.use('/api/products', router)
