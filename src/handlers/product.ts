9d23b1d32e2d3d1df63a38b2ddab447bc6f577e8
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-27 18:23:11 +0200

    feat: implement getProducts handler and update router for product retrieval

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
+export const getProducts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const products = await Product.findAll({
+    order: [["id", "ASC"]],
+    attributes: { exclude: ["createdAt", "updatedAt", "availability"] },
+  });
+  res.json({ data: products });
+};
+
 export const createProduct = async (
   req: Request,
   res: Response
--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { createProduct } from "./handlers/product";
+import { createProduct, getProducts } from "./handlers/product";
 import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
 // Routing
-router.get("/", (req, res) => {
-  const auth = true;
-  res.json("Desde get");
-});
+router.get("/", getProducts);
 router.post(
   "/",
   body("name")
