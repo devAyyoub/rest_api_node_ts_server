7e345e092fa3e605f56a3b423100b8b767ed00f2
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-29 18:29:38 +0200

    test: enhance product creation tests with additional validation scenarios

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,17 +2,50 @@ import request from "supertest";
 import server from "../../server";
 
 describe("POST /api/products", () => {
+    test("should display validation errors", async () => {
+      const response = await request(server).post("/api/products").send({});
+  
+      expect(response.statusCode).not.toBe(201);
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(response.body.errors).toHaveLength(4);
+    });
+
+    test("should validate that the price is greater than 0", async () => {
+      const response = await request(server).post("/api/products").send({
+        name : "test",
+        price : -1
+      });
+  
+      expect(response.statusCode).not.toBe(201);
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(response.body.errors).toHaveLength(1);
+    });
+
+    test("should validate that the price is a number and greater than 0", async () => {
+      const response = await request(server).post("/api/products").send({
+        name : "test",
+        price : "hola"
+      });
+  
+      expect(response.statusCode).not.toBe(201);
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toHaveProperty("errors");
+      expect(response.body.errors).toHaveLength(2);
+    });
+    
   test("should create a new product", async () => {
     const response = await request(server).post("/api/products").send({
       name: "Mouse - Testing",
       price: 50,
     });
 
-    expect(response.statusCode).toBe(201)
-    expect(response.body).toHaveProperty('data')
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveProperty("data");
 
-    expect(response.statusCode).not.toBe(200)
-    expect(response.statusCode).not.toBe(404)
-    expect(response.body).not.toHaveProperty('error')
+    expect(response.statusCode).not.toBe(200);
+    expect(response.statusCode).not.toBe(404);
+    expect(response.body).not.toHaveProperty("errors");
   });
 });
