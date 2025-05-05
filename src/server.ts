ac6d7922d27cddab86d41476b18f054d28191801
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-05 20:22:19 +0200

    feat: add CORS support to the server
    - Added `cors` package to handle Cross-Origin Resource Sharing.
    - Configured CORS options to allow requests from a specific frontend URL.
    - Updated `package.json` and `package-lock.json` to include `cors` and its types.
    - Modified `server.ts` to use CORS middleware for handling requests.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import colors from "colors";
+import cors, { CorsOptions } from "cors";
 import swaggerUI from "swagger-ui-express";
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 import router from "./router";
 import db from "./config/db";
-import colors from "colors";
 
 // conectar a base de datos
 export async function connectDB() {
@@ -21,11 +22,28 @@ connectDB();
 // Instancia de express
 const server = express();
 
+// Permitir conexiones
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de CORS'))
+    }
+  },
+};
+
+server.use(cors(corsOptions));
+
 // Leer datos de formularios
 server.use(express.json());
 
 server.use("/api/products", router);
 
-server.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec, swaggerUiOptions));
+server.use(
+  "/docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerSpec, swaggerUiOptions)
+);
 
 export default server;
